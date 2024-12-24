d558e5805be9dca79f2dfd1f126d65d95b177c43
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-12-24 17:18:03 -0300

    consegui
    consegui

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -38,6 +38,7 @@ export default function CreatProject() {
           title: title,
           description: description,
           date: date,
+          anotation: [],
         },
       ];
     });
--- src/pages/ProjectDetail.jsx
+++ src/pages/ProjectDetail.jsx
@@ -3,28 +3,20 @@ import { useOutletContext } from "react-router";
 import { useRef } from "react";
 
 export default function ProjectDetail() {
-  // let callId = useId();
-  // let [anotation, setAnotation] = useState([{ id: "", message: "" }]);
-  // function attAnotation(e) {
-  //   setAnotation((prevValue) => {
-  //     return [...prevValue, { id: callId, message: e.target.value }];
-  //   });
-  // }
   const { formCreate, setFormCreate } = useOutletContext();
   const myParams = useParams();
   let anotationRef = useRef();
 
   const addAnnotation = (e) => {
     e.preventDefault();
+    const annotaitionValue = anotationRef.current.value;
+    if (!annotaitionValue.trim()) return;
     setFormCreate((prevValue) => {
       return prevValue.map((project) => {
         if (String(project.id) === myParams.id) {
           return {
             ...project,
-            anotation: [
-              ...(project.anotation || []),
-              anotationRef.current.value,
-            ],
+            anotation: [...project.anotation, annotaitionValue],
           };
         }
         return project;
@@ -32,7 +24,6 @@ export default function ProjectDetail() {
     });
     anotationRef.current.value = "";
   };
-  console.log("formCreat" + formCreate);
   let arrData = formCreate?.find((el) => String(el.id) === myParams.id);
   console.log(arrData);
   return (
