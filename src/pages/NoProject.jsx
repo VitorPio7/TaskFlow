688514b6cd7f22f071d2789519cee19d1ecb320d
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-12-26 17:23:13 -0300

    terceiro
    terceiro

--- src/pages/NoProject.jsx
+++ src/pages/NoProject.jsx
@@ -2,10 +2,13 @@ import { NavLink } from "react-router";
 
 export default function NoProject() {
   return (
-    <div className="flex flex-col">
-      <img src="logo.png" alt="logo" />
-      <h1>No Project Selected</h1>
-      <button>
+    <div className="gap-7 flex flex-col w-full items-center justify-center">
+      <img src="logo.png" alt="logo" className="w-36" />
+      <h1 className="text-4xl font-bold">No Project Selected</h1>
+      <p className="text-2xl text-gray">
+        Select a project or get started with a new one
+      </p>
+      <button className="text-xl w-80 h-14 bg-black text-white rounded-lg ">
         <NavLink to="/">Create new project</NavLink>
       </button>
     </div>
